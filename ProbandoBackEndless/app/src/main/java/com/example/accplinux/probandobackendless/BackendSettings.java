75b7e34216542c771b2aa028b5c8d792b13118d7
alvaro <alvarocasadocc@gmail.com>
2016-11-02 21:36:21 +0100

    login y registro funcionando correctamente

---
+++ ProbandoBackEndless/app/src/main/java/com/example/accplinux/probandobackendless/BackendSettings.java
@@ -0,0 +1,11 @@
+package com.example.accplinux.probandobackendless;
+
+/**
+ * Created by accplinux on 1/11/16.
+ */
+
+public class BackendSettings {
+    public static String APP_ID = "1BF00D70-57EA-B78B-FFEA-0329E7E68200";
+    public static String SECRET_KEY = "F6A09F64-D438-1AE7-FF11-11F48E039A00";
+    public static String VERSION = "v1";
+}
--- ProbandoBackEndless/app/src/main/java/com/example/accplinux/probandobackendless/MainActivity.java
+++ ProbandoBackEndless/app/src/main/java/com/example/accplinux/probandobackendless/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.accplinux.probandobackendless;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
@@ -12,52 +18,116 @@ import com.backendless.exceptions.BackendlessFault;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String APP_ID = "1BF00D70-57EA-B78B-FFEA-0329E7E68200";
-    public static String SECRET_KEY = "F6A09F64-D438-1AE7-FF11-11F48E039A00";
-    public static String VERSION = "v1";
+    Button bR,bL;
+    EditText ed1,ed2;
+    TextView tx1;
+    int counter=3;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Backendless.initApp(this, APP_ID, SECRET_KEY, VERSION );
 
-        registerUserAsync();
+        Backendless.initApp(this, BackendSettings.APP_ID, BackendSettings.SECRET_KEY, BackendSettings.VERSION);
+
+        bL=(Button)findViewById(R.id.buttonLogin);
+        bR=(Button)findViewById(R.id.buttonRegister);
+        ed1=(EditText)findViewById(R.id.usuario);
+        ed2=(EditText)findViewById(R.id.password);
+
+      //  tx1.setVisibility(View.GONE);
+
+        bR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUserAsync();
+            }
+        });
+        bL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserAsync();
+            }
+        });
+
+
+        //registerUserAsync();
 
     }
 
 
-    private static void registerUserAsync()
-        {
+    private void registerUserAsync() {
+
+        final BackendlessUser user = new BackendlessUser();
+
+        user.setEmail(ed1.getText().toString());
+        user.setPassword( ed2.getText().toString());
+
             AsyncCallback<BackendlessUser> callback = new AsyncCallback<BackendlessUser>()
             {
                 @Override
                 public void handleResponse( BackendlessUser registeredUser )
                 {
                     System.out.println( "User has been registered - " + registeredUser.getObjectId() );
+
+                    Backendless.Persistence.save( new Comment( "Se ha producido un nuevo registro : "+user.getEmail()+".", user.getEmail() ), new BackendlessCallback<Comment>()
+                    {
+                        @Override
+                        public void handleResponse( Comment comment )
+                        {
+                            Log.i( "Comments", "Got new comment from " + user.getEmail() );
+                        }
+                    } );
                 }
 
                 @Override
                 public void handleFault( BackendlessFault backendlessFault )
                 {
-                    System.out.println( "Server reported an error - " + backendlessFault.getMessage() );
+                    System.out.println( "Server reported an error - " + backendlessFault.getMessage());
+                    Toast.makeText(getApplicationContext(), backendlessFault.getMessage(),Toast.LENGTH_LONG).show();
                 }
             };
 
-            final BackendlessUser user = new BackendlessUser();
-            user.setEmail( "alvarocc.ac@gmail.com" );
-            user.setPassword( "a1a2a3a4" );
-            user.setProperty( "phoneNumber", "666-666-666" );
+            //user.setProperty( "phoneNumber", "666-666-666" );
 
-            Backendless.UserService.register( user, callback );
+        Backendless.UserService.register( user, callback );
 
-            Backendless.Persistence.save( new Comment( "Funciona!!!!", user.getEmail() ), new BackendlessCallback<Comment>()
+
+    }
+    public void loginUserAsync(){
+        final BackendlessUser user = new BackendlessUser();
+
+        user.setEmail(ed1.getText().toString());
+        user.setPassword( ed2.getText().toString());
+
+        AsyncCallback<BackendlessUser> callback = new AsyncCallback<BackendlessUser>()
+        {
+            @Override
+            public void handleResponse( BackendlessUser loggedUser )
             {
-                @Override
-                public void handleResponse( Comment comment )
-                {
-                    Log.i( "Comments", "Got new comment from " + user.getEmail() );
-                }
-            } );
-        }
+                System.out.println( "User logged - " + loggedUser.getObjectId() );
+                Toast.makeText(getApplicationContext(), "Bienvenido.",Toast.LENGTH_LONG).show();
+
+                Backendless.Persistence.save( new Comment( "Se ha producido un nuevo login en la aplicación de : "+user.getEmail()+".",
+                        user.getEmail() ), new BackendlessCallback<Comment>() {
+                    @Override
+                    public void handleResponse( Comment comment )
+                    {
+                        Log.i( "Comments", "Got new login from " + user.getEmail() );
+                    }
+                } );
+            }
+
+            @Override
+            public void handleFault( BackendlessFault backendlessFault )
+            {
+                System.out.println( "Server reported an error - " + backendlessFault.getMessage());
+                Toast.makeText(getApplicationContext(), backendlessFault.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        };
+        Backendless.UserService.login(user.getEmail(),user.getPassword(),callback);
+
+    }
 }
