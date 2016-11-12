68bea90e3fd8f212511b727042e6d83fa884c542
alvaro <alvarocasadocc@gmail.com>
2016-11-12 17:52:30 +0100

    menu principal con bootstrap funcionando

--- proyectoBootstrap/app/src/main/java/com/example/accplinux/probandobackendless/MenuPrincipal.java
+++ proyectoBootstrap/app/src/main/java/com/example/accplinux/probandobackendless/MenuPrincipal.java
@@ -6,17 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.beardedhen.androidbootstrap.BootstrapCircleThumbnail;
+
 public class MenuPrincipal extends AppCompatActivity {
 
-    ImageView empleados;
-    ImageView proyectos;
+    BootstrapCircleThumbnail empleados;
+    BootstrapCircleThumbnail proyectos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-        empleados = (ImageView) findViewById(R.id.empleados);
-        proyectos = (ImageView) findViewById(R.id.proyectos);
+        empleados = (BootstrapCircleThumbnail) findViewById(R.id.empleados);
+        proyectos = (BootstrapCircleThumbnail) findViewById(R.id.proyectos);
 
         empleados.setOnClickListener(new View.OnClickListener() {
             @Override
